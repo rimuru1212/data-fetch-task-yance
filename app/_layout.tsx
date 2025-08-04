123bfedeca62089e5c66b4a39244a04a9b107164
rimuru <yancemagoncia2@gmail.com>
2025-08-04 19:02:53 +0800

    saved

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ export default function RootLayout() {
   return (
       <Stack>
         <Stack.Screen name="index" options={{
-            headerTitle: "Magoncia Online Store"
+            headerTitle: "Magoncia Y Online Store"
         }}/>
       </Stack>
   );
